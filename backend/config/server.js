2956ed50815fbff5f0686179db6c437c2bbb216e
Chad Miller <chadtmiller15@gmail.com>
2021-02-21 23:55:30 -0800

    dont change that

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "api.studsnstuff.dev",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "f3bbae485ea8f4ec67db5022f14ecd20"),
