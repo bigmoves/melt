146e07a07f321fea3bb4545c6e06329703c3d8a7
Chad Miller <chadtmiller15@gmail.com>
2021-02-21 23:52:58 -0800

    ssl

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -1,36 +1,5 @@
 module.exports = {
   settings: {
-    // cors: {
-    //   enabled: true,
-    //   headers: "*",
-    //   origin: ["*"],
-    //   // origin: [
-    //   //   "http://localhost:3000",
-    //   //   "http://64.227.109.182",
-    //   //   "https://melt-chadtmiller15.vercel.app",
-    //   //   "https://melt.vercel.app",
-    //   // ],
-    // },
-    cors: {
-      enabled: true,
-      origin: "*",
-      expose: [
-        "WWW-Authenticate",
-        "Server-Authorization",
-        "Access-Control-Expose-Headers",
-      ],
-      maxAge: 31536000,
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-      headers: [
-        "Content-Type",
-        "Authorization",
-        "X-Frame-Options",
-        "Origin",
-        "Access-Control-Allow-Headers",
-        "access-control-allow-origin",
-      ],
-    },
     parser: {
       enabled: true,
       multipart: true,
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "api.studsnstuff.dev",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f3bbae485ea8f4ec67db5022f14ecd20'),
+      secret: env("ADMIN_JWT_SECRET", "f3bbae485ea8f4ec67db5022f14ecd20"),
     },
   },
 });
