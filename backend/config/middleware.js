3664328e5e4459595f85a02a08cb62788b86c6eb
Chad Miller <chadtmiller15@gmail.com>
2021-02-21 22:58:28 -0800

    getting sleepy

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -12,12 +12,23 @@ module.exports = {
     //   // ],
     // },
     cors: {
+      enabled: true,
+      origin: "*",
+      expose: [
+        "WWW-Authenticate",
+        "Server-Authorization",
+        "Access-Control-Expose-Headers",
+      ],
+      maxAge: 31536000,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
       headers: [
         "Content-Type",
         "Authorization",
+        "X-Frame-Options",
         "Origin",
-        "Accept",
-        "Cache-Control",
+        "Access-Control-Allow-Headers",
+        "access-control-allow-origin",
       ],
     },
     parser: {
